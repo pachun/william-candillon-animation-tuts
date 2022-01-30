bb09b0e53ac9cf75365f5d3a88cb63d25f275eff
Nick Pachulski <nick@pachulski.me>
2022-01-30 03:46:13 -0500

    Move something around w/o remembering start pos

--- App.tsx
+++ App.tsx
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native"
+import { PanGestureHandler } from "react-native-gesture-handler"
+import Animated, {
+  useAnimatedGestureHandler,
+  useSharedValue,
+  useAnimatedStyle,
+} from "react-native-reanimated"
 
 export default function App() {
+  const translateX = useSharedValue(0)
+  const translateY = useSharedValue(0)
+  const onGestureEvent = useAnimatedGestureHandler({
+    onActive: (event, context) => {
+      translateX.value = event.translationX
+      translateY.value = event.translationY
+    },
+  })
+  const style = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: translateX.value },
+        { translateY: translateY.value },
+      ],
+    }
+  })
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <PanGestureHandler {...{ onGestureEvent }}>
+        <Animated.View {...{ style }}>
+          <View style={styles.square} />
+        </Animated.View>
+      </PanGestureHandler>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
-});
+  square: {
+    width: 300,
+    height: 150,
+    backgroundColor: "#666",
+  },
+})
